bfce84ffad38aa75c2738ae55f9dfdfa6b225c8c
Shane Rose <shane.rose@intersystems.com>
2017-07-12 12:06:45 -0400

    Intitial commit for Players.htm
    Update to get routes working

--- EH/views/app.js
+++ EH/views/app.js
@@ -1,3 +1,7 @@
+
+var url = 'http://localhost:57773/csp/games/rest/'
+var urlEH = url + 'eh/';
+
 var app = angular.module('EH', ['ngRoute']);
 
 app.config(function($routeProvider) {
@@ -5,6 +9,10 @@ app.config(function($routeProvider) {
     .when("/ancientones", {
         templateUrl : "AncientOnes.htm",
         controller : "ancientones"
+    })
+    .when("/players", {
+        templateUrl : "Players.htm",
+        controller : "players"
     })
     .when("/characters", {
         templateUrl : "Characters.htm",
@@ -14,15 +22,52 @@ app.config(function($routeProvider) {
 
 
 app.controller('ancientones', function($scope, $http) {
-    $http.get('http://localhost:57773/csp/games/rest/eh/ancientones').
+
+    $scope.fetch = function() {
+      $http.get(urlEH + 'ancientones').
         then(function(response) {
             $scope.ancientones = response.data;
         });
+    };
+
+    $scope.submit = function() {
+      $http.post(urlEH + 'ancientones/' + $scope.newName)
+      $scope.fetch();
+    };
+
+    $scope.fetch();
 });
 
 app.controller('characters', function($scope, $http) {
-    $http.get('http://localhost:57773/csp/games/rest/eh/characters').
+
+    $scope.fetch = function() {
+    $http.get(urlEH + 'characters').
         then(function(response) {
             $scope.characters = response.data;
         });
+    };
+
+    $scope.submit = function() {
+      $http.post(urlEH + 'characters/' + $scope.newName)
+      $scope.fetch();
+    };
+
+    $scope.fetch();
+});
+
+app.controller('players', function($scope, $http) {
+
+    $scope.fetch = function() {
+    $http.get(url + 'players').
+        then(function(response) {
+            $scope.players = response.data;
+        });
+    };
+
+    $scope.submit = function() {
+      $http.post(url + 'players/' + $scope.newName)
+      $scope.fetch();
+    };
+
+    $scope.fetch();
 });
