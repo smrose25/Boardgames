194b6402c1c1a16ee79903fdd79e6d25c34b1565
Shane Rose <shane.rose@intersystems.com>
2017-11-22 20:44:29 -0500

    Changed get to post

--- EH/js/service.api.js
+++ EH/js/service.api.js
@@ -16,7 +16,7 @@ angular.module('EH').service('api',function($http) {
             return $http.get('/csp/games/rest/players')
         },
         newPlayer: function(name) {
-            return $http.get('/csp/games/rest/players/' + name)
+            return $http.post('/csp/games/rest/players/' + name)
         },
         newSession: function(data) {
             return $http.post('/csp/games/rest/eh/sessions', data)
