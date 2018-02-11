c24e4ca935f57469e75adef5f36c7d3d853a703d
Shane Rose <shane.rose@intersystems.com>
2018-02-10 23:09:39 -0500

    Added characters and submit for new session.
    Added rumours for viewing sessions

--- EH/js/ctrl.newsession.js
+++ EH/js/ctrl.newsession.js
@@ -10,19 +10,40 @@ angular.module('EH').controller('newsession', ['$scope','$state','$stateParams',
       api.getPlayers().then(function(response) {
           $scope.players = angular.copy(response.data);
       });
+
+      $scope.count = 0;
+      $scope.session = {};
+      $scope.session.characters = [];
     };
 
-    $scope.addPlayer = function() {
+    $scope.removeCharacter = function() {
+      if ($scope.count > 1) {
+        $scope.count--;
+        $("tr:last-child").remove();
+        $scope.session.characters.pop();
+      }
 
     };
 
     $scope.addCharacter = function() {
-
+      $scope.count++;
+      $scope.session.characters.push({'index':$scope.count,'player':'','character':'','survived':'0'});
     };
 
     $scope.submit = function() {
 
+      var p = [];
+      for (var i = 0; i < $scope.session.characters.length; i++) {
+        p.push($scope.session.characters[i].player);
+      }
+      $scope.session.players = Array.from(new Set(p));
+
+      api.newSession($scope.session).then(function(response) {
+        $state.go('sessions');
+      });
+
     };
 
     $scope.fetch();
+
 }]);
